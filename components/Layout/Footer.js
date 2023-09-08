f8b8004822a2f15b4fdf0e67280c61d46f34699f
sarmiente <53012993+sarmiente@users.noreply.github.com>
2023-09-08 12:54:46 -0500

    1 file modified

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="dark:bg-black text-gray-700 body-font md:pt-20">
+    <footer className="dark:bg-black text-gray-700 body-font md:pt-20 ">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 dark:text-white">
           <span className="ml-3 text-3xl font-logo">Carlos Sarmiento</span>
