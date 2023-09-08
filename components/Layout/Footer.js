0a164cb41f2931dffa63071d1004c48c25bdc0c3
sarmiente <53012993+sarmiente@users.noreply.github.com>
2023-09-08 12:55:28 -0500

    1 file modified

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
           <span className="ml-3 text-3xl font-logo">Carlos Sarmiento</span>
         </a>
         <p className="text-sm dark:text-white text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2   sm:mt-0 mt-4">
-          © 2022
+          © 2023
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
           <a
@@ -27,22 +27,7 @@ export default function Footer() {
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z" />
             </svg>
           </a>
-          <a
-            href="https://twitter.com/sarmiente_"
-            target="_blank"
-            className="ml-3 text-gray-500 dark:text-white"
-          >
-            <svg
-              fill="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z" />
-            </svg>
-          </a>
+
           <a
             href="https://www.instagram.com/sarmiente/"
             target="_blank"
